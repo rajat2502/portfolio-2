2a3fcfbe131e64631f7f305645f4f98b401b800f
abc <rajat2502@users.noreply.github.com>
2019-04-15 02:25:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+$(".home").click(function(){
+  $(".grid-home").css("display","block");
+  $(".grid-work").css("display","none");
+  $(".grid-resume").css("display","none");
+  $(".grid-contact").css("display","none");
+});
+
+$(".resume").click(function(){
+  $(".grid-resume").css("display","block");
+  $(".grid-work").css("display","none");
+  $(".grid-home").css("display","none");
+  $(".grid-contact").css("display","none");
+});
+
+$(".work").click(function(){
+  $(".grid-work").css("display","block");
+  $(".grid-home").css("display","none");
+  $(".grid-resume").css("display","none");
+  $(".grid-contact").css("display","none");
+});
+
+$(".contact").click(function(){
+  $(".grid-contact").css("display","block");
+  $(".grid-work").css("display","none");
+  $(".grid-resume").css("display","none");
+  $(".grid-resume").css("display","none");
+});
